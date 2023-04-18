8622d53c9ee39aa70975df67bb49b4ffc897dc11
Walid Ahmeddin <112585598+walidata1@users.noreply.github.com>
2023-04-18 02:20:00 +0200

    Comments added to functions

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function labb1_theme_support(){
   add_theme_support('post-thumbnails');
   add_theme_support('widgets');
 
-  // Registrera ett widgetområde
+  // Registrera widgetområde
   register_sidebar([
 
     'name' => 'Testwidgetarea',
@@ -19,25 +19,23 @@ function labb1_theme_support(){
     'description' => 'Footer Widget',
     'id' => 'widget2'
   ]);
-
-
-
 }
 
 add_action('after_setup_theme', 'labb1_theme_support');
 
-
+// Registrera stylesheets
 function labb1_register_styles(){
 
   $version = wp_get_theme()->get( 'Version' );
-wp_enqueue_style('labb1-fontawesome', get_template_directory_uri(). "./assets/css/font-awesome.css", array(), $version, 'all');
-wp_enqueue_style('labb1-bootstrap', get_template_directory_uri(). "./assets/css/bootstrap.css", array(), $version, 'all');
-wp_enqueue_style('labb1-style', get_template_directory_uri(). "./assets/css/style.css", array(), $version, 'all');
+  wp_enqueue_style('labb1-fontawesome', get_template_directory_uri(). "./assets/css/font-awesome.css", array(), $version, 'all');
+  wp_enqueue_style('labb1-bootstrap', get_template_directory_uri(). "./assets/css/bootstrap.css", array(), $version, 'all');
+  wp_enqueue_style('labb1-style', get_template_directory_uri(). "./assets/css/style.css", array(), $version, 'all');
 
 }
 
 add_action( 'wp_enqueue_scripts', 'labb1_register_styles');
 
+// Registrera Js Jquery filer
 function labb1_register_scripts(){
 
   wp_enqueue_script('labb1-jquery', get_template_directory_uri()."/assets/js/jquery.js", array(), '5.0', false );
@@ -47,15 +45,16 @@ function labb1_register_scripts(){
 
 add_action( 'wp_enqueue_scripts', 'labb1_register_scripts');
 
+// Registrera menyer
 function labb1_menus(){
 
   $locations = array(
-    'primary' => "Desktop Primary Top",
-    'side-menu' => "Side Menu Items",
-    'sidebar-menu' => "Sidebar Menu Items"
+  'primary' => "Desktop Primary Top",
+  'side-menu' => "Side Menu Items",
+  'sidebar-menu' => "Sidebar Menu Items"
   );
-    register_nav_menus($locations);
-    
+
+  register_nav_menus($locations); 
   }
 
 add_action('init', 'labb1_menus');
